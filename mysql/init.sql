5e5f543628731a4ec0bfe31b999b6feb006bce97
phan huu cuong <phanhuucuong05012001@gmail.com>
2023-03-06 01:07:42 +0700

    update env

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,22 +1,21 @@
+CREATE DATABASE ggsheet;
 
-CREATE DATABASE mydatabase;
+use ggsheet;
 
-USE mydatabase;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `fanpage_id` varchar(50) NOT NULL,
+  `sheet_id` varchar(255) NOT NULL,
+  `sheet_name` varchar(255) NOT NULL,
+  `sdt_column` varchar(50) NOT NULL,
+  `madon_column` varchar(50) NOT NULL,
+  `start_column` varchar(50) NOT NULL,
+  `end_column` varchar(50) NOT NULL,
+  `key_fanpage` varchar(255) NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
-CREATE TABLE mytable (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(50),
-  email VARCHAR(50),
-  PRIMARY KEY (id)
-);
 
-INSERT INTO mytable (name, email) VALUES
-  ('John Doe', 'john.doe@example.com'),
-  ('Jane Doe', 'jane.doe@example.com');
-
-CREATE TABLE mysql.plugin (
-    name varchar(64) COLLATE utf8mb4_0900_ai_ci NOT NULL,
-    dl   varchar(128) COLLATE utf8mb4_0900_ai_ci NOT NULL,
-    PRIMARY KEY (name)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+INSERT INTO `user` (`id`, `fanpage_id`, `sheet_id`, `sheet_name`, `sdt_column`, `madon_column`, `start_column`, `end_column`, `key_fanpage`, `status`) VALUES
+(1, '321365132', 'dhas654322df984asdsheet', 'data1', 'A2:A', 'D2:D', 'A', 'D', 'tokenaslkdalksdasdasdasdasd', 1),
+(2, '321654132151321', 'sheetnamecong51321651', 'banhang', 'B2:B', 'F2:F', 'B', 'H', '', 1);
