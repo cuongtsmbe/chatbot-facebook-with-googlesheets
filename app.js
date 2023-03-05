fc5a18265f113725b4dfa1ac77ef782bb80d9b79
phan huu cuong <phanhuucuong05012001@gmail.com>
2023-03-05 12:51:53 +0700

    fix app.js

--- app.js
+++ app.js
@@ -4,19 +4,18 @@ require('dotenv').config();
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const cors=require('cors');
-
-  
 const Webhooks = require("./webhooks/webhooks");
 
+
 // parse application/x-www-form-urlencoded
- app.use(bodyParser.urlencoded({ extended: false }))
- // parse application/json
- app.use(bodyParser.json())
- app.use(cors({
-   origin: ["https://www.facebook.com"],
-   methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD','DELETE'],
-   credentials: true
- }));
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+app.use(cors({
+  origin: ["https://www.facebook.com"],
+  methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD','DELETE'],
+  credentials: true
+}));
 
 app.get("/about",function(req,res,next){
     res.send(`/3/2023.Google sheet + chatbot fanpage FB.`);
