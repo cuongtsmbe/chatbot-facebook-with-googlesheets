a54a456f0ce25c51c083583902c04c0925e4a6f3
phan huu cuong <phanhuucuong05012001@gmail.com>
2023-03-06 01:10:04 +0700

    mysql init.sql

--- mysql/init.sql
+++ mysql/init.sql
@@ -17,5 +17,5 @@ CREATE TABLE `user` (
 
 
 INSERT INTO `user` (`id`, `fanpage_id`, `sheet_id`, `sheet_name`, `sdt_column`, `madon_column`, `start_column`, `end_column`, `key_fanpage`, `status`) VALUES
-(1, '321365132', 'dhas654322df984asdsheet', 'data1', 'A2:A', 'D2:D', 'A', 'D', 'tokenaslkdalksdasdasdasdasd', 1),
-(2, '321654132151321', 'sheetnamecong51321651', 'banhang', 'B2:B', 'F2:F', 'B', 'H', '', 1);
+(1, '104099862250569', '17KwoPtSv5mx77c0PFg8VGLzZfyajCo-SDNNBiC1ltuw', 'data1', 'D2:D', 'E2:E', 'A', 'E', 'EAAIg0JI8QGoBAP6PG87fclzWV9G3DqDngNEbfgfdwNF4UVsJoYtswKg9ENGrheTLa7tZBl4NcRKGwNoDf2WCwr0KnKTDvzibrZCaZB8Oc4o31UjIDRiBzJZAaYHxZAL9diCE90mqtHjzyEBs49a2eL3Q1e2xhYtKzAj4KL5cqaqSctbni9oQi', 1),
+(2, '225038468945199', '100ebDMj7T8S3_sX9IftGdJ1AF7soH3OUYVVIfHKGDR4', 'banhang', 'G6:G', 'H6:H', 'D', 'H', 'EAAIg0JI8QGoBAN7hqeZCUZBMxtQyPjZCUZCuSlIPCqwDegQMhFyze5shKmM0mSZC8nXghSWjfyyhKw8kCTlDj31wIIQkBQBI54haaPJgTku95wrobH7I57wYtZAMW7Pgn1bKUmRlnSmNzAhPCdpDPzqAha0zabVJZBXQvFA4uK1d9MkqSDKMlVd', 1);
